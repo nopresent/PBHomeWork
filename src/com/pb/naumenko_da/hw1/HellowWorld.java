f8ad38bb9834d5beb53158db926ce4b950f4707d
Naumenko Dmitrij <nopresent@gmail.com>
2021-10-08 23:19:04 +0300

    Initial commit 2

--- src/com/pb/naumenko_da/hw1/HellowWorld.java
+++ src/com/pb/naumenko_da/hw1/HellowWorld.java
@@ -3,5 +3,6 @@ package com.pb.naumenko_da.hw1;
 public class HellowWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Hello World");
     }
 }

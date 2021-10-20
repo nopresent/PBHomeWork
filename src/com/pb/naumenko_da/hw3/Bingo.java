5b9bd2b4ded6edb881f118f52ecfbedfb23a2da3
Naumenko Dmitrij <nopresent@gmail.com>
2021-10-20 18:59:06 +0300

    Домашка N3.1

---
+++ src/com/pb/naumenko_da/hw3/Bingo.java
@@ -0,0 +1,76 @@
+package com.pb.naumenko_da.hw3;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Bingo {
+    public static void main(String[] args) {
+
+        // Записали рандомное число
+        Random randome = new Random();
+        int storeRnd = randome.nextInt(101);
+
+        int userInput, startCount = 0, endCout = 100;
+        int userCountTry = 1;
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("********\n Bingo! Угадай Мело... Угадай Число от 0 до 100 \n********");
+
+        while (userCountTry > 0) {
+
+            System.out.print("Введите число от " + startCount + " до " + endCout + ": ");
+            userInput = scan.nextInt();
+
+            if (userInput > 100) {
+                // Игра закончена, бла, бла, бла
+                System.out.println("Спасибо за игру!");
+                break;
+            } else {
+
+                if (storeRnd == userInput) {
+                    System.out.print("Подздравляю, ты угадал загаднное мной число! ");
+                    break;
+                } else if (storeRnd > userInput) {
+                    startCount = ++userInput;
+                    if (check(startCount, endCout, storeRnd)) {
+                        break;
+                    } else {
+                        System.out.println("Загаданное мной число больше " + userInput);
+                    }
+                } else {
+                    endCout = --userInput;
+                    if (check(startCount, endCout, storeRnd)) {
+                        break;
+                    } else {
+                        System.out.println("Загаданное мной число меньше " + userInput);
+                    }
+                }
+
+            }
+
+            userCountTry++;
+
+            if (userCountTry > 2) {
+                System.out.println("******** Для выхода укажите любое число больше 100");
+            } else {
+                System.out.println("********");
+            }
+            System.out.println();
+
+        }
+
+        System.out.println("Кол-во использованных попыток " + userCountTry + " шт.");
+
+
+    }
+
+    public static boolean check(int startCount, int endCout, int storeRnd) {
+
+        if (startCount == endCout) {
+            System.out.println("Подздравляю, методом исключения ;-) ты угадал число " + storeRnd + "!");
+            return true;
+        }
+
+        return false;
+    }
+}

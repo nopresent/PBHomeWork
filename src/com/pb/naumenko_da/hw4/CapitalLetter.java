36996bfc66f05f9d87a69bce0e2f59fca66c187a
Naumenko Dmitrij <nopresent@gmail.com>
2021-10-24 19:43:59 +0300

    Домашка N4.1

---
+++ src/com/pb/naumenko_da/hw4/CapitalLetter.java
@@ -0,0 +1,43 @@
+package com.pb.naumenko_da.hw4;
+
+import java.util.Scanner;
+
+public class CapitalLetter {
+    public static void main(String[] args) {
+        String userInputString;
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Напишите немного о погоде! \nНапример: За !окном , осень ,хочется спать! :-)");
+        userInputString = scan.nextLine();
+
+        System.out.println(toCapitalLetter(userInputString));
+
+    }
+
+    static String toCapitalLetter(String string) {
+        String[] subString = string.split(" ");
+        StringBuilder returnString = new StringBuilder();
+
+        for (int i = 0; i < subString.length; i++) {
+            returnString.append(firstUpperCase(subString[i]));
+
+            if (i != subString.length - 1) {
+                returnString.append(" ");
+            }
+        }
+        return returnString.toString();
+    }
+
+    static String firstUpperCase(String string) {
+        StringBuilder outBody = new StringBuilder();
+        for (int i = 0; i < string.length(); i++) {
+            if (string.substring(i, i + 1).matches("[а-яА-Яa-zA-Z]")) {
+                outBody.append(string.substring(i, i + 1).toUpperCase()).append(string.substring(i + 1));
+                break;
+            }
+            outBody.append(string.substring(i, i + 1));
+        }
+        return outBody.toString();
+    }
+}
+

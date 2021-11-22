3cea7afbfa19a4c56f2bb9309c5d0e263b1935b6
Naumenko Dmitrij <nopresent@gmail.com>
2021-11-22 18:35:07 +0200

    Домашка N9

--- src/com/pb/naumenko_da/hw9/FileNumbers.java
+++ src/com/pb/naumenko_da/hw9/FileNumbers.java
@@ -1,24 +1,27 @@
 package com.pb.naumenko_da.hw9;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Random;
+import java.util.Scanner;
 
 public class FileNumbers {
 
     public static void main(String[] args) {
 
-        String fileName = "numbers.txt";
-        createNumbersFile(fileName);
+        createNumbersFile();
+        createOddNumbersFile();
 
     }
 
-    public static void createNumbersFile(String fileName) {
+    public static void createNumbersFile() {
 
+        String fileName = "numbers.txt";
         Path path = Paths.get(fileName);
+
         Random rand = new Random();
 
         try (BufferedWriter writer = Files.newBufferedWriter(path)) {
@@ -33,7 +36,7 @@ public class FileNumbers {
             System.out.println("Write to file: " + path.toAbsolutePath() + " - done!");
 
         } catch (Exception ex) {
-            // e.printStackTrace();
+            // ex.printStackTrace();
             System.out.println("Error with file write: " + ex);
             // Добавить логирование, если не смогли открыть файл для записи
         }
@@ -41,7 +44,41 @@ public class FileNumbers {
     }
 
     public static void createOddNumbersFile() {
+        String fileName = "numbers.txt";
+        Path path = Paths.get(fileName);
 
+        String oddFileName = "odd-numbers.txt";
+        Path oddPath = Paths.get(oddFileName);
+
+        try (BufferedReader reader = Files.newBufferedReader(path); BufferedWriter writer = Files.newBufferedWriter(oddPath)) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                writer.write(oddToZero(line));
+                writer.newLine();
+            }
+
+            System.out.println("Write to file: " + oddPath.toAbsolutePath() + " - done!");
+        } catch (Exception ex) {
+            // ex.printStackTrace();
+            System.out.println("Error with file reader: " + ex);
+            // Добавить логирование, если не смогли открыть файл для записи
+        }
+    }
+
+    public static String oddToZero(String string) {
+        StringBuilder strBuffer = new StringBuilder();
+        int nextNumber;
+        Scanner scan = new Scanner(string);
+        scan.useDelimiter(" ");
+        while (scan.hasNext()) {
+            nextNumber = scan.nextInt();
+            if (nextNumber % 2 == 0) {
+                strBuffer.append("0 ");
+            } else {
+                strBuffer.append(nextNumber + " ");
+            }
+        }
+        return strBuffer.toString();
     }
 
 }

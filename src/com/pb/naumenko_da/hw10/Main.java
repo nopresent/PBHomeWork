2ea4bb98551f26f96b80907fb17d1eb6c840097f
Naumenko Dmitrij <nopresent@gmail.com>
2021-11-27 19:47:14 +0200

    Домашка N10 Вроде все учел )

--- src/com/pb/naumenko_da/hw10/Main.java
+++ src/com/pb/naumenko_da/hw10/Main.java
@@ -3,14 +3,14 @@ package com.pb.naumenko_da.hw10;
 public class Main {
     public static void main(String[] args) {
 
-        NumBox numBox = new NumBox(10);
+        NumBox<Integer> numBox = new NumBox(10);
 
         numBox.add(5);
         numBox.add(8);
         numBox.add(6);
         numBox.add(7);
 
-        System.out.println("Массив integer: " + numBox);
+        System.out.println("Массив Integer: " + numBox);
         System.out.println("Получили елемент с индексом 3: " + numBox.get(3));
         System.out.println("Длинна массива: " + numBox.length());
         System.out.println("Среднее арифметическое: " + numBox.average());
@@ -19,15 +19,15 @@ public class Main {
 
         System.out.println("****************************************");
 
-        NumBox numBox1 = new NumBox(10);
+        NumBox<Float> numBox1 = new NumBox(10);
 
-        numBox1.add(5.7878787);
-        numBox1.add(8.00092);
-        numBox1.add(10.100500);
-        numBox1.add(6.12);
-        numBox1.add(7.11);
+        numBox1.add(5.7878787f);
+        numBox1.add(8.00092f);
+        numBox1.add(10.100500f);
+        numBox1.add(6.12f);
+        numBox1.add(7.11f);
 
-        System.out.println("Массив double: " + numBox1);
+        System.out.println("Массив Float: " + numBox1);
         System.out.println("Получили елемент с индексом 3: " + numBox1.get(3));
         System.out.println("Длинна массива: " + numBox1.length());
         System.out.println("Среднее арифметическое: " + numBox1.average());
@@ -36,7 +36,7 @@ public class Main {
 
         System.out.println("****************************************");
 
-        NumBox numBox2 = new NumBox(2);
+        NumBox<Integer> numBox2 = new NumBox(2);
         System.out.println("В случае если массив полон - выбросить исключение.");
         System.out.println("Создали массив на 2 елемента, пробуем положить в него 3 елемента");
 
